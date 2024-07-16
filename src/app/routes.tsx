0f46ca5eb8db3ed4caee47fe29d2ebe474945718
alex-abanin <aa@occam.fi>
2024-07-16 15:22:44 +0300

    main route requires snap

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        element: <AuthGuard isMetamaskNeeded />,
+        element: <AuthGuard isMetamaskNeeded isSnapNeeded />,
         children: [
           {
             path: "/",
