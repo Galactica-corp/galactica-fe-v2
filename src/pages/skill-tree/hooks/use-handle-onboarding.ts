940c1786f691c7bf3ab00c0a011fecfd52bc4122
alex-abanin <aa@occam.fi>
2024-07-19 13:35:06 +0300

    ids

--- src/pages/skill-tree/hooks/use-handle-onboarding.ts
+++ src/pages/skill-tree/hooks/use-handle-onboarding.ts
@@ -1,3 +1,4 @@
+import { useCerts } from "entities/cert";
 import { useCompleteQuestMutation } from "shared/api";
 import { useInstallSnapMutation } from "shared/snap/rq";
 import { catchError } from "shared/ui/toast";
@@ -7,6 +8,7 @@ import { Quest } from "../types";
 const section = "1-onboarding";
 
 export const useHandleOnboarding = () => {
+  const { certs } = useCerts();
   const mutation = useCompleteQuestMutation();
   const installSnapMutation = useInstallSnapMutation();
 
@@ -22,6 +24,13 @@ export const useHandleOnboarding = () => {
 
       if (quest.id === "install-snap") {
         await installSnapMutation.mutateAsync({});
+        await mutation.mutateAsync({
+          quest: quest.id,
+          section,
+        });
+      }
+
+      if (quest.id === "pass-kyc" && certs.find((c) => c.standard === "gip1")) {
         await mutation.mutateAsync({
           quest: quest.id,
           section,
