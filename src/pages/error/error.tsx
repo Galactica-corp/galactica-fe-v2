379ac7dc856734e141206f88bd2ec3c76dca4e0c
alex-abanin <aa@occam.fi>
2024-07-23 11:54:56 +0300

    error page

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,8 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+
+import { useSessionStorage } from "@uidotdev/usehooks";
 
 import { Button } from "shared/ui/button";
 
 export const ErrorPage = () => {
+  const error = useRouteError();
+
+  const [reloadCount, setReloadCount] = useSessionStorage<number>(
+    "error-reload-count",
+    0
+  );
+
+  console.log(error);
+
+  if (
+    error instanceof Error &&
+    error.message.includes("Failed to fetch dynamically imported module") &&
+    error.message.includes("Importing a module script failed") &&
+    reloadCount < 3
+  ) {
+    setReloadCount(reloadCount + 1);
+    window.location.reload();
+  }
+
   return (
     <div
       className="flex grow flex-col items-center justify-center"
