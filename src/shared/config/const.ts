e72aac69b0f76a61e3b2f47c4c629e1c7d3223ec
alex-abanin <aa@occam.fi>
2024-08-02 18:17:51 +0300

    hach

--- src/shared/config/const.ts
+++ src/shared/config/const.ts
@@ -3,9 +3,15 @@ import { Chain } from "viem";
 import { galacticaAndromeda } from "./networks/galactica-andromeda";
 import { galacticaReticulumTestnet } from "./networks/galactica-reticulum";
 
-export const supportedChains = [
-  galacticaAndromeda,
-  galacticaReticulumTestnet,
-].filter(
-  (chain) => chain.id === Number.parseInt(import.meta.env.VITE_CHAIN_ID)
-) as [Chain, ...Chain[]];
+// export const supportedChains = [
+//   galacticaAndromeda,
+//   galacticaReticulumTestnet,
+// ].filter(
+//   (chain) => chain.id === Number.parseInt(import.meta.env.VITE_CHAIN_ID)
+// ) as [Chain, ...Chain[]];
+
+export const supportedChains: [Chain, ...Chain[]] = [
+  window.location.hostname.includes("stage")
+    ? galacticaAndromeda
+    : galacticaReticulumTestnet,
+];
